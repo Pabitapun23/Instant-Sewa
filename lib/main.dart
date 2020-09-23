03a8786ae2dc0237f48e48e555379a19ce4ccfe8
Pabita <pabita.pun23@gmail.com>
2020-09-23 16:08:57 +0545

    profile new changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:instantsewa/model/profile_model.dart';
 import 'package:instantsewa/services/handymans_service.dart';
+import 'package:instantsewa/services/profiles_service.dart';
 import 'package:instantsewa/services/service_providers_service.dart';
 
 import 'ui/home_page.dart';
@@ -8,6 +10,7 @@ void setup()
 {
   GetIt.instance.registerSingleton<HandyMansService>(HandyMansService());
   GetIt.instance.registerSingleton<ServiceProvidersService>(ServiceProvidersService());
+  GetIt.instance.registerSingleton<ProfilesService>(ProfilesService());
 }
 
 void main() {
--- lib/model/profile_model.dart
+++ lib/model/profile_model.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 
 class Profile{        //model = database
   String name;
+  String username;
   String img;
   String address;
   String phone_number;
   String email;
 
-  Profile({this.name, this.img, this.address, this.phone_number, this.email});         //{} optional
+  Profile({this.name, this.username, this.img, this.address, this.phone_number, this.email});         //{} optional
 }
