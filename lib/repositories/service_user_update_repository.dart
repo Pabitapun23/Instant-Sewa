073466bcfff6b2488e38140a8c50a5ce7272b6bb
Bishal Devkota <devkotahancy@gmail.com>
2020-10-30 00:08:53 +0545

    Service User Update Repository and State

---
+++ lib/repositories/service_user_update_repository.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:instantsewa/application/InstantSewa_api.dart';
+import 'package:instantsewa/application/classes/errors/common_error.dart';
+import 'package:instantsewa/application/storage/localstorage.dart';
+import 'package:instantsewa/application/storage/storage_keys.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class ServiceUserUpdateRepository
+{
+  Future<bool> updateFullName({@required String fullName});
+  Future<bool> updateAddress({@required String address});
+  Future<bool> updatePhone({@required String phoneNo});
+}
+
+class ServiceUserUpdateRepositoryImpl implements ServiceUserUpdateRepository
+{
+  @override
+  Future<bool> updateAddress({String address})
+  async
+  {
+    String id;
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var user = jsonDecode(localStorage.getString('user'));
+    id = user['id'].toString();
+    try {
+      Dio dio = new Dio();
+      Response response = await InstantSewaAPI.dio
+          .post("/addressUpdate", data: {
+        "id": id,
+        "address": address
+      }, options: Options(headers: {
+        'Authorization': "Bearer ${LocalStorage.getItem(TOKEN)}"
+      }));
+      return true;
+    } on DioError catch (e) {
+      showNetworkError(e);
+      return false;
+    }
+  }
+
+  @override
+  Future<bool> updateFullName({String fullName}) async
+  {
+    String id;
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var user = jsonDecode(localStorage.getString('user'));
+    id = user['id'].toString();
+    try {
+      Dio dio = new Dio();
+      Response response = await InstantSewaAPI.dio
+          .post("/fullnameUpdate", data: {
+        "id": id,
+        "fullname": fullName
+      }, options: Options(headers: {
+        'Authorization': "Bearer ${LocalStorage.getItem(TOKEN)}"
+      }));
+      return true;
+    } on DioError catch (e) {
+      showNetworkError(e);
+      return false;
+    }
+  }
+
+  @override
+  Future<bool> updatePhone({String phoneNo}) async
+  {
+    String id;
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var user = jsonDecode(localStorage.getString('user'));
+    id = user['id'].toString();
+    try {
+      Dio dio = new Dio();
+      Response response = await InstantSewaAPI.dio
+          .post("/fullnameUpdate", data: {
+        "id": id,
+        "phoneno": phoneNo
+      }, options: Options(headers: {
+        'Authorization': "Bearer ${LocalStorage.getItem(TOKEN)}"
+      }));
+      return true;
+    } on DioError catch (e) {
+      showNetworkError(e);
+      return false;
+    }
+  }
+
+}
---
+++ lib/state/service_user_update_state.dart
@@ -0,0 +1,38 @@
+import 'package:instantsewa/repositories/service_user_update_repository.dart';
+
+class ServiceUserUpdateState
+{
+  final ServiceUserUpdateRepository _serviceUserUpdateRepository;
+  ServiceUserUpdateState(this._serviceUserUpdateRepository):assert(_serviceUserUpdateRepository!=null);
+  Future<bool> updateAddress({String address})
+  async
+  {
+    if(await _serviceUserUpdateRepository.updateAddress(address: address))
+      {
+        return true;
+      }
+    else{
+      return false;
+    }
+  }
+  Future<bool> updateFullName({String fullName})
+  async
+  {
+    if(await _serviceUserUpdateRepository.updateFullName(fullName: fullName)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  Future<bool> updatephoneNo({String phoneNo})
+  async
+  {
+    if(await _serviceUserUpdateRepository.updatePhone(phoneNo: phoneNo)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
