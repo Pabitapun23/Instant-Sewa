3514af4fe032af2afddac621418605566cecc9f7
Amrita <amritapoudel016@gmail.com>
2020-11-22 14:16:12 +0545

    tracking page

---
+++ lib/ui/ongoing_page.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:instantsewa/util/hexcode.dart';
+
+class OngoingPage extends StatefulWidget {
+  @override
+  _OngoingPageState createState() => _OngoingPageState();
+}
+
+class _OngoingPageState extends State<OngoingPage> {
+  Color _purple = HexColor('#603f8b');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart Name'),
+        centerTitle: true,
+        backgroundColor: _purple,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.qr_code_scanner_sharp,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Container(
+              height: (MediaQuery.of(context).size.height) * 0.20,
+              decoration: BoxDecoration(
+                color: Colors.white10,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Card(
+                elevation: 0.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Item Name',
+                            style: GoogleFonts.openSans(
+                              textStyle: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 19,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'Price',
+                            style: GoogleFonts.openSans(
+                              textStyle: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'Quantity',
+                            style: GoogleFonts.openSans(
+                              textStyle: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Status',
+                            style: GoogleFonts.openSans(
+                              textStyle: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          Icon(
+                            Icons.check_circle,
+                            color: Colors.green[700],
+                            size: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
